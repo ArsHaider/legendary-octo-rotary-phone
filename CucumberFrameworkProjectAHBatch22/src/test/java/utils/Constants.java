fc49c5fff6d21406c8cc04ed438e1cf2d3797b88
ArsHaider <104385372+ArsHaider@users.noreply.github.com>
2025-05-10 20:09:30 -0400

    Delete CucumberFrameworkProjectAHBatch22 directory

--- CucumberFrameworkProjectAHBatch22/src/test/java/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class Constants {
-    public static final String CONFIG_FILE_PATH = System.getProperty("user.dir") + "/src/test/resources/config/config.properties";
-    public static final int EXPLICIT_WAIT = 22;
-    public static final int IMPLICIT_WAIT = 11;
-    public static final String SCREENSHOT_FILE_PATH = System.getProperty("user.dir") + "/screenshots/";
-}
